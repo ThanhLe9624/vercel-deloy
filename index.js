20f1107e085cc382868719503069377e5be71413
ThanhLe9624 <114575764+ThanhLe9624@users.noreply.github.com>
2023-09-24 14:07:01 +0700

    final

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 // Import packages
 const express = require("express");
 const home = require("./routes/home");
-
+let path = require('path');
 // Middlewares
 const app = express();
 app.use(express.json());
 
+// view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
 // Routes
 app.use("/home", home);
 
 // connection
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
+
+//connect db
+const mongoose = require('mongoose')
+const URL = "mongodb+srv://bestsuovn:3KQoggmjehcSKISH@cluster0.recfwmr.mongodb.net/product?retryWrites=true&w=majority";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      URL,
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    console.log('Connected to mongoDB')
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
+
+connectDB()
--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const abc = mongoose.Schema({
+  fullname: String,
+  email: String,
+  password: String,
+  image: String
+});
+
+const CustomerModel = mongoose.model('customer', abc);
 
 router.get("/", async (req, res, next) => {
-  return res.status(200).json({
-    title: "Express Testing",
-    message: "The app is working properly!",
-  });
+  
+  try {
+    let custs = await CustomerModel.find();
+  return res.render('index', {customers: custs});
+  } catch (error) {
+    console.log(error);
+    return null
+  }
 });
+
 
 module.exports = router;
